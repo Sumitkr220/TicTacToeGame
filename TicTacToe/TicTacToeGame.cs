6a67b3f4a2a742bc450c5106aee50f52a4c4fae4
sumitpandit <sumitpandit178@gmail.com>
2020-10-13 09:51:32 +0530

    [Sumit] add: Creating Board for TicTacToe

---
+++ TicTacToe/TicTacToeGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    public class TicTacToeGame
+    {
+        public char[] createBoardTicTacToe()
+        {
+            char[] board = new char[10];
+            int MAX_SIZE = 10;
+            int i = 0;
+            while(MAX_SIZE-->0)
+            {
+                board[i] = ' ';
+                i++;
+            }
+            return board;
+        }
+    }
+}
